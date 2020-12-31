d1e3d71f9ec06610be3175c73dde599105a09fe8
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2020-12-31 21:21:59 +0900

    Remove test code from Rust source

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,8 @@
 use rutie::{Module, Object};
+use whatlang;
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Init_whatlang() {
     Module::new("Whatlang").define(|_itself| {});
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
