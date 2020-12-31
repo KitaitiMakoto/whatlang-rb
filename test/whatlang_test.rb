1226e63cd5f5d933cbf3be7350b42494328ae625
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2020-12-31 21:34:48 +0900

    Remove poilerplated tests

--- test/whatlang_test.rb
+++ test/whatlang_test.rb
@@ -3,13 +3,4 @@
 require_relative "helper"
 
 class WhatlangTest < Test::Unit::TestCase
-  test "VERSION" do
-    assert do
-      ::Whatlang.const_defined?(:VERSION)
-    end
-  end
-
-  test "something useful" do
-    assert_equal("expected", "actual")
-  end
 end
